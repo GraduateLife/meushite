65c7e92064ed2a2898bc2a21dd5b21824e9d047f
first-glance-admin <edwdzzzytt@gmail.com>
2024-12-20 00:53:22 +0800

    changed favicon

--- lib/post_utils/settings.ts
+++ lib/post_utils/settings.ts
@@ -10,23 +10,3 @@ export const fuseOptions = {
   keys: ['title', 'content', 'keywords'],
   threshold: 0.8,
 };
-//NOTE - cannot use this in the blog page because it's an async function, nodejs doesn't recognize it
-// export async function markdownToHtml(content: string) {
-//   const result = await unified()
-//     .use(remarkParse)
-//     .use(remarkGfm)
-//     .use(remarkToc, {
-//       heading: 'Table of Contents', // This will be the TOC heading
-//       tight: true, // More compact list
-//     })
-//     .use(remarkRehype)
-//     .use(rehypeSlug)
-//     .use(rehypePrettyCode, {
-//       theme: 'one-dark-pro',
-//       keepBackground: true,
-//     })
-//     .use(rehypeStringify)
-//     .process(content);
-
-//   return String(result);
-// }
--- next.config.ts
+++ next.config.ts
@@ -1,26 +1,8 @@
 import type { NextConfig } from 'next';
-import { generateSearchIndex } from './lib/post_utils/indexer';
-import { copyPostImages } from './lib/post_utils/copyImages';
 
-// class ContentProcessingPlugin {
-//   static pluginName = 'ContentProcessingPlugin';
-
-//   apply(compiler: any) {
-//     // Use emit hook instead of beforeRun
-//     // emit runs right before emitting assets to output directory
-//     compiler.hooks.emit.tapPromise(
-//       ContentProcessingPlugin.pluginName,
-//       async (compilation: any) => {
-//         try {
-//           copyPostImages();
-//           generateSearchIndex();
-//         } catch (error) {
-//           compilation.errors.push(error as Error);
-//         }
-//       }
-//     );
-//   }
-// }
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 
 const nextConfig: NextConfig = {
   output: process.env.DOCKER_BUILD === 'true' ? 'standalone' : undefined,
@@ -41,4 +23,4 @@ const nextConfig: NextConfig = {
 };
 
 // Merge MDX config with Next.js config
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);

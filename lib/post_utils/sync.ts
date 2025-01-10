de517693aae035f1aa7bdf5a04ab021dbe61b116
Apple Pie <>
2025-01-10 16:37:41 +0800

    test image sync

--- lib/post_utils/sync.ts
+++ lib/post_utils/sync.ts
@@ -15,14 +15,20 @@ async function syncAssociatedImages(mdFilePath: string): Promise<void> {
     /\.(jpg|jpeg|png|gif|webp|svg)$/i.test(file)
   );
 
-  if (imageFiles.length > 0) {
-    echo.info(
-      `Syncing ${notice(imageFiles.length.toString())} images from ${notice(
-        dirPath
-      )}`
-    );
-    await uploadFolder(dirPath, dirPath);
+  for (const filePath of imageFiles) {
+    const exists = await hasObjectInBucket(filePath);
+
+    if (!exists) {
+      echo.info(`Creating post sync record: ${notice(filePath)}`);
+      await uploadObject(filePath, filePath);
+      await syncAssociatedImages(filePath);
+    } else {
+      echo.info(`Updating post sync record: ${notice(filePath)}`);
+      await updateObject(filePath, filePath);
+      await syncAssociatedImages(filePath);
+    }
   }
+  await uploadFolder(dirPath, dirPath);
 }
 
 export async function syncChangedPosts(filesFromHook: string[]): Promise<void> {
